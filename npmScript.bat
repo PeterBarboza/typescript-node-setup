23003f1005bc4ed89a0c17935b0877f8fadead1c
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-07-11 19:21:15 -0300

    scripts to download dependencies added

---
+++ npmScript.bat
@@ -0,0 +1,3 @@
+npm init -y
+npm install --save-dev @babel/cli @babel/core @babel/node @babel/preset-env @babel/preset-typescript @types/express @types/jest @typescript-eslint/eslint-plugin @typescript-eslint/parser babel-plugin-module-resolver eslint eslint-config-standard eslint-plugin-import eslint-plugin-n eslint-plugin-promise jest ts-jest ts-node-dev tsconfig-paths typescript
+pause
